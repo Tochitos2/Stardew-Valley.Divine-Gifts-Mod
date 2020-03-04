218adf6e5e6cc1fc98f675f8c86df4cd296865bf
Tomas De Haro Angus <tochitos2@gmail.com>
2020-03-04 23:16:46 +0000

    Added framework for handling item donation.

--- Class1.cs
+++ Class1.cs
@@ -11,9 +11,61 @@ namespace YobaGifts
     {
         public override void Entry(IModHelper helper)
         {
-            
+            // Event handlers will be added here.
+        }
+
+        /**
+         * Handles an item offering to Yoba.
+         */
+        public void ItemHandler(Item item)
+        {
+            if (item.getCategoryName() == "Trash")
+            {
+                AddRandomBadEvent();
+                return;
+            }
+
+            var value = GetItemValue(item);
+            AddRandomGoodEvent(value);
+
+        }
+
+        /**
+         * Causes a random bad event to occur to the player who donated the item. The higher the offering 
+         * value the higher the chance of better events occuring. 
+         */
+        private static void AddRandomGoodEvent(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        /**
+         * Causes a random bad event to occur to the player who donated the item.
+         */
+        private static void AddRandomBadEvent()
+        {
+            throw new NotImplementedException();
         }
         
-        
+        /**
+         * Calculates the value of an item based on its base value and its quality.
+         */
+        private static int GetItemValue(Item item)
+        {
+            int value;
+            var sellPrice = item.salePrice();
+            switch (item.getCategoryName())
+            {
+                case "Cooking":
+                    value = sellPrice * 2; // Yoba likes a personal touch.
+                    break;
+                
+                default:
+                    value = sellPrice;
+                    break;
+            }
+
+            return value;
+        }
     }
 }
