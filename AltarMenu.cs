e6f7c07951f94fa219e26d6edacfd96532506c31
Tomas De Haro Angus <tochitos2@gmail.com>
2020-03-18 04:10:57 +0000

    Added class for altar menu.

---
+++ AltarMenu.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using StardewValley.Menus;
+using StardewModdingAPI;
+
+namespace YobaGifts
+{
+    internal class AltarMenu : MenuWithInventory
+    {
+        public static Texture2D MenuTexture;
+        private readonly ModEntry modEntry;
+        private string descriptionText;
+        private ClickableTextureComponent donationSlot;
+        private ClickableTextureComponent donateButton;
+
+        public AltarMenu(ModEntry mod)
+        {
+            this.modEntry = mod;
+            this.descriptionText = "Offer up an item to Yoba in exchange for his favour.";
+        }
+    }
+}
